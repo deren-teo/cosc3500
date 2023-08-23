20e4ded6d5022a492971f225f9d930b8e44c2130
Deren Teo <s4528554@student.uq.edu.au>
2023-08-23 21:11:43 +1000

    proj: bug fixes and optimisations
    Fixed bugs surround early stopping conditions and optimised grid local
    sum method.

--- project/game_of_life.cc
+++ project/game_of_life.cc
@@ -39,50 +39,49 @@ static void GridRandomInit(uint8_t *grid, const int n_rows, const int n_cols) {
 
 /**
  * Returns the sum of the 3x3 grid of cells aronud and including (row, col).
- * If row or col is on the edge of the grid, the 3x3 grid wraps around to the
- * other side of the grid, as if on a taurus.
+ * Cells outside the grid are treated as zero (dead).
  *
  * @param grid Pointer to the memory allocated for the grid
  * @param row Row number of centre cell
  * @param col Column number of centre cell
+ * @param n_rows Number of rows in the grid
  * @param n_cols Number of columns in the grid
  *
  * @return Sum of the 3x3 grid of cells centred on (row, col).
 */
 static int GridLocalSum(uint8_t *grid, const int row, const int col,
                         const int n_rows, const int n_cols) {
-    // Surrounding row and column indices with wraparound logic
-    int row0 = (row - 1 + n_rows) % n_rows;
-    int row2 = (row + 1) % n_rows;
-    int col0 = (col - 1 + n_cols) % n_cols;
-    int col2 = (col + 1) % n_cols;
-
     int sum = 0;
-    sum += (grid[row0 * n_cols + col0]);
-    sum += (grid[row0 * n_cols + col ]);
-    sum += (grid[row0 * n_cols + col2]);
-    sum += (grid[row  * n_cols + col0]);
-    sum += (grid[row  * n_cols + col ]);
-    sum += (grid[row  * n_cols + col2]);
-    sum += (grid[row2 * n_cols + col0]);
-    sum += (grid[row2 * n_cols + col ]);
-    sum += (grid[row2 * n_cols + col2]);
+    int idx00 = (row - 1) * n_cols + (col - 1);
+    int idx10 = idx00 + n_cols;
+    int idx20 = idx10 + n_cols;
+    if (row > 0) {
+        if (col > 0) {
+            sum += grid[idx00];
+        }
+        sum += grid[idx00 + 1];
+        if (col < (n_cols - 1)) {
+            sum += grid[idx00 + 2];
+        }
+    }
+    if (col > 0) {
+        sum += grid[idx10];
+    }
+    sum += grid[idx10 + 1];
+    if (col < (n_cols - 1)) {
+        sum += grid[idx10 + 2];
+    }
+    if (row < (n_rows - 1)) {
+        if (col > 0) {
+            sum += grid[idx20];
+        }
+        sum += grid[idx20 + 1];
+        if (col < (n_cols - 1)) {
+            sum += grid[idx20 + 2];
+        }
+    }
+    std::cout << sum;
     return sum;
-}
-
-/**
- * Get the binary state of the cell (i, j).
- *
- * @param grid Pointer to the memory allocated for the grid
- * @param row Row number of the cell
- * @param col Column number of the cell
- * @param n_cols Number of columns in the grid
- *
- * @return 1 for alive, 0 for dead.
-*/
-static inline char GridGetState(uint8_t *grid, const int row, const int col,
-                                const int n_cols) {
-    return grid[row * n_cols + col];
 }
 
 /**
@@ -92,10 +91,13 @@ static inline char GridGetState(uint8_t *grid, const int row, const int col,
  * @param row Row number of the cell
  * @param col Column number of the cell
  * @param n_cols Number of columns in the grid
+ * @param state_changed Pointer to variable to whether state has just changed
 */
-static inline void GridSetState(uint8_t *grid, const int row, const int col,
-                                const int n_cols, uint8_t state) {
-    grid[row * n_cols + col] = state;
+static void CellSetState(uint8_t *grid, const int row, const int col,
+        const int n_cols, uint8_t state, int *state_changed) {
+    int idx = row * n_cols + col;
+    *state_changed = (grid[idx] != state);
+    grid[idx] = state;
 }
 
 /**
@@ -105,7 +107,7 @@ static inline void GridSetState(uint8_t *grid, const int row, const int col,
  * @param n_rows Number of rows in the grid
  * @param n_cols Number of columns in the grid
  *
- * @return 1 if all cells in the grid are dead (boolean false).
+ * @return 0 if successful. 1 if no cell states changed (signals early stop).
 */
 static int GridEvolve(uint8_t *grid, const int n_rows, const int n_cols) {
     // Allocate copy of grid to hold intermediate evolved cell states
@@ -113,35 +115,44 @@ static int GridEvolve(uint8_t *grid, const int n_rows, const int n_cols) {
         std::malloc(n_rows * n_cols * sizeof(uint8_t)));
     std::memcpy(evolved_grid, grid, n_rows * n_cols * sizeof(uint8_t));
 
-    // Keep track of whether any cells are alive
-    bool any_alive = false;
+    // Early stopping condition: no cells changed (possibly all dead)
+    int stop_early = 1;
 
     // Update cell states in grid copy
+    int state_changed;
     for (unsigned short i = 0; i < n_rows; i++) {
         for (unsigned short j = 0; j < n_cols; j++) {
-            // TODO: should probably use a 3x3 kernel
+            // TODO: use a 3x3 kernel
             switch (GridLocalSum(grid, i, j, n_rows, n_cols)) {
+                case 0: {
+                    // All cells around (i, j) are dead; possibly early stop
+                    break;
+                }
                 case 3: {
-                    GridSetState(grid, i, j, n_cols, 1);
-                    any_alive = true;
+                    // Cell (i, j) now alive, regardless of previous state
+                    CellSetState(grid, i, j, n_cols, 1, &state_changed);
+                    stop_early &= !state_changed;
                     break;
                 }
                 case 4: {
-                    any_alive |= (GridGetState(grid, i, j, n_cols) == 1);
+                    // (i, j) does not change state; possible early stop
                     break;
                 }
                 default: {
-                    GridSetState(grid, i, j, n_cols, 0);
+                    // Cell (i, j) now dead, regardless of previous state
+                    CellSetState(grid, i, j, n_cols, 0, &state_changed);
+                    stop_early &= !state_changed;
                     break;
                 }
             }
         }
     }
     // Copy evolved cell states back into grid and free temporary grid copy
+    // TODO: highly memory intensive; is there any way to point `grid` at
+    //  `evolved_grid` and free the old grid?
     std::memcpy(grid, evolved_grid, n_rows * n_cols * sizeof(uint8_t));
     free(evolved_grid);
-
-    return static_cast<int>(any_alive);
+    return stop_early;
 }
 
 int main(int argc, char *argv[]) {
