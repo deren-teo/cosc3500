447b64c59a8d702bf1f05c0055a4b062a420093f
Deren Teo <teo.deren@icloud.com>
2023-09-06 12:04:53 +1000

    proj: bookkeeping changes

--- project/s_profile.sh
+++ project/s_profile.sh
@@ -10,5 +10,9 @@
 
 make clean
 make prof
-./game_of_life -i 1000 -s 1000x1000 -v
-gprof ./game_of_life gmon.out > profile.txt
+./game_of_life -i 1000 --seed 1 -s 1000x1000 -v
+gprof ./game_of_life gmon.out > exp1-profile.txt
+./game_of_life -i 1000 --seed 2 -s 1000x1000 -v
+gprof ./game_of_life gmon.out > exp2-profile.txt
+./game_of_life -i 1000 --seed 42 -s 1000x1000 -v
+gprof ./game_of_life gmon.out > exp3-profile.txt
