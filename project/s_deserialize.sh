689820ddca965c1a49546de74165da02969540ba
Deren Teo <teo.deren@icloud.com>
2023-09-08 09:35:37 +1000

    proj: create deserialize batch script

---
+++ project/s_deserialize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=s4528554-benchmark-life
+#SBATCH --partition=cosc
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G # memory
+#SBATCH --time=0-00:10 # time (D-HH:MM)
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+
+conda activate coscenv
+python deserializer.py game_of_life.out 120x120 > game_of_life.txt
