cc5c3353e8280a7bf589e141b8e61fddbf4aaa3c
Deren Teo <teo.deren@icloud.com>
2023-09-18 11:12:55 +1000

    asgn: implemented cache blocking
    Used block size of 64, found to be faster than 32, 128 and 256.
    Approx. 30% speedup (1.3x) over no cache blocking for N=512.

--- assignment/matrixMultiply.cpp
+++ assignment/matrixMultiply.cpp
@@ -16,14 +16,27 @@ void matrixMultiply(int N, const float* A, const float* B, float* C, int* args, 
     // Clear any existing values in C before calculating new values
     memset(C, 0, sizeof(float) * N * N);
 
-    for (int j = 0; j < N; j++)
+    // Cache blocking using block sizes of 64
+    for (int j = 0; j < N; j += 64)
     {
-        for (int k = 0; k < N; k++)
+        for (int k = 0; k < N; k += 64)
         {
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < N; i += 64)
             {
-                // Fortran order
-                C[i + j * N] += A[i + k * N] * B[k + j * N];
+                for (int jj = 0; jj < 64; jj++)
+                {
+                    int J = j + jj;
+                    for (int kk = 0; kk < 64; kk++)
+                    {
+                        int K = k + kk;
+                        for (int ii = 0; ii < 64; ii++)
+                        {
+                            int I = i + ii;
+                            // Fortran order
+                            C[I + J * N] += A[I + K * N] * B[K + J * N];
+                        }
+                    }
+                }
             }
         }
     }
