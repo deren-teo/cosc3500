6b27467facd70471635ca6d5257573fce517357c
Deren Teo <teo.deren@icloud.com>
2023-09-20 21:53:07 +1000

    asgn: multi-threading over cache blocking loops
    Implemented OpenMP multi-threading over (all three) cache blocking
    loops. Approx. 4x speedup over only SIMD vectoriation.
    
    Also some rearranging of SIMD calls, but these didn't affect speed.

--- assignment/matrixMultiply.cpp
+++ assignment/matrixMultiply.cpp
@@ -23,20 +23,21 @@ void matrixMultiply(int N, const float* A, const float* B, float* C, int* args, 
         {
             for (int i = 0; i < N; i += 64)
             {
+                #pragma omp parallel for
                 for (int jj = 0; jj < 64; jj++)
                 {
                     int J = j + jj;
                     for (int kk = 0; kk < 64; kk++)
                     {
                         int K = k + kk;
+                        __m256 b = _mm256_set1_ps(B[K + J * N]);
                         for (int ii = 0; ii < 64; ii += 8)
                         {
                             int I = i + ii;
                             // Fortran order
                             __m256 a = _mm256_loadu_ps(A + I + K * N);
-                            __m256 b = _mm256_set1_ps(B[K + J * N]);
-                            __m256 x = _mm256_mul_ps(a, b);
                             __m256 c = _mm256_loadu_ps(C + I + J * N);
+                            __m256 x = _mm256_mul_ps(a, b);
                             c = _mm256_add_ps(c, x);
                             _mm256_storeu_ps(C + I + J * N, c);
                         }
