fb40af7a8c0a208893eb6f7cafbf210b936a7136
Deren Teo <s4528554@student.uq.edu.au>
2023-09-22 14:21:41 +1000

    asgn: 3 separate optimisations
    Moved "parallel for" region to over outermost loop (from over only
    3 innermost cache-blocking loops). Approx. 5% speedup over previous.
    
    Changed SIMD unaligned loads and stores to aligned loads and stores.
    Approx. 3x speedup over unaligned.
    
    Completely unrolled innermost loop to better pipeline loads, which have
    a latency of 7 cycles. Approx. 9% speedup.
    
    Altogether, CPU code now runs within approx. 2.1x speed of Intel MKL.
    Should be well within 2.3x cutoff for max grade, but more optimisation
    likely possible to increase the safety margin.

--- assignment/matrixMultiply.cpp
+++ assignment/matrixMultiply.cpp
@@ -17,13 +17,13 @@ void matrixMultiply(int N, const float* A, const float* B, float* C, int* args, 
     memset(C, 0, sizeof(float) * N * N);
 
     // Cache blocking using block sizes of 64
+    #pragma omp parallel for
     for (int j = 0; j < N; j += 64)
     {
         for (int k = 0; k < N; k += 64)
         {
             for (int i = 0; i < N; i += 64)
             {
-                #pragma omp parallel for
                 for (int jj = 0; jj < 64; jj++)
                 {
                     int J = j + jj;
@@ -31,16 +31,52 @@ void matrixMultiply(int N, const float* A, const float* B, float* C, int* args, 
                     {
                         int K = k + kk;
                         __m256 b = _mm256_set1_ps(B[K + J * N]);
-                        for (int ii = 0; ii < 64; ii += 8)
-                        {
-                            int I = i + ii;
-                            // Fortran order
-                            __m256 a = _mm256_loadu_ps(A + I + K * N);
-                            __m256 c = _mm256_loadu_ps(C + I + J * N);
-                            __m256 x = _mm256_mul_ps(a, b);
-                            c = _mm256_add_ps(c, x);
-                            _mm256_storeu_ps(C + I + J * N, c);
-                        }
+
+                        // Fortran order
+                        __m256 a1 = _mm256_load_ps(A + i + K * N);
+                        __m256 a2 = _mm256_load_ps(A + i + 8 + K * N);
+                        __m256 a3 = _mm256_load_ps(A + i + 16 + K * N);
+                        __m256 a4 = _mm256_load_ps(A + i + 24 + K * N);
+                        __m256 a5 = _mm256_load_ps(A + i + 32 + K * N);
+                        __m256 a6 = _mm256_load_ps(A + i + 40 + K * N);
+                        __m256 a7 = _mm256_load_ps(A + i + 48 + K * N);
+                        __m256 a8 = _mm256_load_ps(A + i + 56 + K * N);
+
+                        __m256 c1 = _mm256_load_ps(C + i + J * N);
+                        __m256 c2 = _mm256_load_ps(C + i + 8 + J * N);
+                        __m256 c3 = _mm256_load_ps(C + i + 16 + J * N);
+                        __m256 c4 = _mm256_load_ps(C + i + 24 + J * N);
+                        __m256 c5 = _mm256_load_ps(C + i + 32 + J * N);
+                        __m256 c6 = _mm256_load_ps(C + i + 40 + J * N);
+                        __m256 c7 = _mm256_load_ps(C + i + 48 + J * N);
+                        __m256 c8 = _mm256_load_ps(C + i + 56 + J * N);
+
+                        __m256 x1 = _mm256_mul_ps(a1, b);
+                        __m256 x2 = _mm256_mul_ps(a2, b);
+                        __m256 x3 = _mm256_mul_ps(a3, b);
+                        __m256 x4 = _mm256_mul_ps(a4, b);
+                        __m256 x5 = _mm256_mul_ps(a5, b);
+                        __m256 x6 = _mm256_mul_ps(a6, b);
+                        __m256 x7 = _mm256_mul_ps(a7, b);
+                        __m256 x8 = _mm256_mul_ps(a8, b);
+
+                        c1 = _mm256_add_ps(c1, x1);
+                        c2 = _mm256_add_ps(c2, x2);
+                        c3 = _mm256_add_ps(c3, x3);
+                        c4 = _mm256_add_ps(c4, x4);
+                        c5 = _mm256_add_ps(c5, x5);
+                        c6 = _mm256_add_ps(c6, x6);
+                        c7 = _mm256_add_ps(c7, x7);
+                        c8 = _mm256_add_ps(c8, x8);
+
+                        _mm256_store_ps(C + i + J * N, c1);
+                        _mm256_store_ps(C + i + 8 + J * N, c2);
+                        _mm256_store_ps(C + i + 16 + J * N, c3);
+                        _mm256_store_ps(C + i + 24 + J * N, c4);
+                        _mm256_store_ps(C + i + 32 + J * N, c5);
+                        _mm256_store_ps(C + i + 40 + J * N, c6);
+                        _mm256_store_ps(C + i + 48 + J * N, c7);
+                        _mm256_store_ps(C + i + 56 + J * N, c8);
                     }
                 }
             }
