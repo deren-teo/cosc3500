b42f130e24d773d99032e8752547e2d5b290ae9c
Deren Teo <teo.deren@icloud.com>
2023-09-26 21:33:59 +1000

    asgn: implemented CUDA
    Calculation result is correct, but fairly slow. Needs significant
    algorithmic improvement to meet top grade band.

--- assignment/matrixMultiplyGPU.cu
+++ assignment/matrixMultiplyGPU.cu
@@ -1,12 +1,34 @@
 #include "matrixMultiplyGPU.cuh"
+#include <iostream> // DEBUG only
+
+// DEBUG only
+void checkError(cudaError_t e)
+{
+    if (e != cudaSuccess)
+    {
+        std::cerr << "CUDA error: " << int(e) << " : " << cudaGetErrorString(e) << '\n';
+        abort();
+    }
+}
 
 __host__ void matrixMultiply_GPU(int N, const float* A, const float* B, float* C, int *arg, int argCount)
 {
-	
+    // NOTE: GradeBot already copies A, B and C to the GPU
+    // Clear any existing values in C before calculating new values
+    checkError(cudaMemset(C, 0, N * N * sizeof(float)));
+
+    int threadsPerBlock = 1024;
+    int numBlocks = N * N / threadsPerBlock;
+    matrixMultiplyKernel_GPU<<<numBlocks, threadsPerBlock>>>(N, A, B, C, 0, 0, 0);
 }
 
 __global__ void matrixMultiplyKernel_GPU(int N, const float* A, const float* B, float* C, int flag0, int flag1, int flag2)
 {
-
+    int z = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = z % N;
+    int j = z / N;
+    for (int k = 0; k < N; k++)
+    {
+        C[z] += A[i + k * N] * B[k + j * N];
+    }
 }
-
